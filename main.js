b7c938f6c9cc2e9ecf01def5b5d697213d25fb02
BenjaminDBallard <benb971@gmail.com>
2023-01-05 13:38:52 -0600

    mastermind logic

--- main.js
+++ main.js
@@ -1,77 +1,104 @@
-'use strict';
+"use strict";
 
-const assert = require('assert');
-const readline = require('readline');
+const assert = require("assert");
+const readline = require("readline");
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
 let board = [];
-let solution = '';
-let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+let solution = "";
+let letters = ["a", "b", "c", "d", "e", "f", "g", "h"];
 
-const printBoard = () =>  {
+const printBoard = () => {
   for (let i = 0; i < board.length; i++) {
     console.log(board[i]);
   }
-}
+};
 
-const generateSolution = () =>  {
+const generateSolution = () => {
   for (let i = 0; i < 4; i++) {
     const randomIndex = getRandomInt(0, letters.length);
     solution += letters[randomIndex];
   }
-}
+};
 
 const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min)) + min;
-}
+};
 
-const generateHint = () =>  {
-  // your code here
-}
+const generateHint = (guess) => {
+  let solutionArray = solution.split("");
+  let guessArray = guess.split("");
+  let correctLetterLocations = 0;
+  for (let i = 0; i < solutionArray.length; i++) {
+    if (guessArray[i] === solutionArray[i]) {
+      correctLetterLocations++;
+      solutionArray[i] = null;
+    }
+  }
+
+  let correctLetters = 0;
+  for (let i = 0; i < solutionArray.length; i++) {
+    let guessedLetter = guessArray[i];
+    let targetIndex = solutionArray.indexOf(guessedLetter);
+    if (targetIndex > -1) {
+      correctLetters++;
+      solutionArray[targetIndex] = null;
+    }
+  }
+  return `${correctLetters}-${correctLetterLocations}`;
+};
 
 const mastermind = (guess) => {
-  solution = 'abcd'; // Comment this out to generate a random solution
-  // your code here
-}
+  // solution = "abcd";
+  if (guess === solution) {
+    let winState = guess + " <= You guessed it!";
+    board.push(winState);
+    return "You guessed it!";
+  } else if (board.length === 10) {
+    let loseState = "you ran out of turns! the solution was: " + solution;
+    board.push(loseState);
+    return "you ran out of turns! the solution was: " + solution;
+  } else {
+    generateHint(guess);
+  }
+  let hint = guess + " " + generateHint(guess);
+  board.push(hint);
+};
 
-
-const getPrompt = () =>  {
-  rl.question('guess: ', (guess) => {
+const getPrompt = () => {
+  rl.question("guess: ", (guess) => {
     mastermind(guess);
     printBoard();
     getPrompt();
   });
-}
+};
 
 // Tests
 
-if (typeof describe === 'function') {
-  solution = 'abcd';
-  describe('#mastermind()', () => {
-    it('should register a guess and generate hints', () => {
-      mastermind('aabb');
+if (typeof describe === "function") {
+  solution = "abcd";
+  describe("#mastermind()", () => {
+    it("should register a guess and generate hints", () => {
+      mastermind("aabb");
       assert.equal(board.length, 1);
     });
-    it('should be able to detect a win', () => {
-      assert.equal(mastermind(solution), 'You guessed it!');
+    it("should be able to detect a win", () => {
+      assert.equal(mastermind(solution), "You guessed it!");
     });
   });
 
-  describe('#generateHint()', () => {
-    it('should generate hints', () => {
-      assert.equal(generateHint('abdc'), '2-2');
+  describe("#generateHint()", () => {
+    it("should generate hints", () => {
+      assert.equal(generateHint("abdc"), "2-2");
     });
-    it('should generate hints if solution has duplicates', () => {
-      assert.equal(generateHint('aabb'), '1-1');
+    it("should generate hints if solution has duplicates", () => {
+      assert.equal(generateHint("aabb"), "1-1");
     });
-
   });
-
 } else {
-
   generateSolution();
   getPrompt();
 }
